c9b020778160d80a41274bf0119326170e4a301a
meghavathshailaja <meghavathshailaja@gmail.com>
2026-08-14 14:22:55 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -17,8 +17,6 @@ public class Factorial {
         int number = 5;
         System.out.println("Factorial of " + number + " is " + compute(number));
     }
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 5d98c23 (third)
+
